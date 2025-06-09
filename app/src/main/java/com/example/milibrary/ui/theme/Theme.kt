79208272b77b2b402dbb718491e7199ad0a74a07
sergi <sergyporto100@hotmail.com>
2025-06-09 18:46:43 +0200

    fix: theme android

--- app/src/main/java/com/example/milibrary/ui/theme/Theme.kt
+++ app/src/main/java/com/example/milibrary/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun MiLibraryTheme(
+fun myLibraryTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
