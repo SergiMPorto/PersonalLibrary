4097e561d0eb73eb0116aa4d48f4290f079628cf
SergiMPorto <121488207+SergiMPorto@users.noreply.github.com>
2026-06-28 17:59:11 +0100

    ci: add build stage with docker compose

---
+++ Backend API/jenkins/build/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "*******Building Backend FastAPI Image*******"
+
+cd "WORKSPACE/Backend API/jenkins/build"
+
+docker compose -f docker-compose.yml build --no-cache
+
+
+echo "****************************"
+echo "** Build completado OK *****"
+echo "****************************"
