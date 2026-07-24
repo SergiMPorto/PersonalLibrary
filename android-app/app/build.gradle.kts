dabb310cb5badd90ae71f8ea6e3583f9021521bd
Sergi <121488207+SergiMPorto@users.noreply.github.com>
2026-07-24 20:09:43 +0200

    feet: add new layout

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -110,6 +110,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.swiperefreshlayout)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
