97fddc063923bbfc4fa235b21857f49e0813dbe3
SergiMPorto <121488207+SergiMPorto@users.noreply.github.com>
2026-08-25 13:18:51 +0100

    fix: remove unnecessary docker tag, image already named correctly

--- backend-api/jenkins/build/build.sh
+++ backend-api/jenkins/build/build.sh
@@ -7,8 +7,7 @@ cd "$WORKSPACE/backend-api/jenkins/build"
 
 docker compose -f docker-compose.yaml build --no-cache
 
-docker tag "personal-library-api:${BUILD_TAG}" "sergimp/milibrary:${BUILD_TAG}"
-docker tag "personal-library-api:${BUILD_TAG}" "sergimp/milibrary:latest"
+
 
 echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push "sergimp/milibrary:${BUILD_TAG}"
