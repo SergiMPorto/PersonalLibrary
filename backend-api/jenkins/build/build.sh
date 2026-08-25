f2ab4ee3371cdb95abb9a4485d7f1957e707e5c4
SergiMPorto <121488207+SergiMPorto@users.noreply.github.com>
2026-08-25 12:37:11 +0100

    fix: correct docker tag and password-stdin flag in build.sh

--- backend-api/jenkins/build/build.sh
+++ backend-api/jenkins/build/build.sh
@@ -7,9 +7,10 @@ cd "$WORKSPACE/backend-api/jenkins/build"
 
 docker compose -f docker-compose.yaml build --no-cache
 
-docker tag "sergimp/milibrary:${BUILD_TAG}" "sergimp/milibrary:latest"
+docker tag "personal-library-api:${BUILD_TAG}" "sergimp/milibrary:${BUILD_TAG}"
+docker tag "personal-library-api:${BUILD_TAG}" "sergimp/milibrary:latest"
 
-echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" -password-stdin
+echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push "sergimp/milibrary:${BUILD_TAG}"
 docker push "sergimp/milibrary:latest"
 
