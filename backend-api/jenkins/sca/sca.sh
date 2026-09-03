0df8a5802fe72a95ce6679b3a3decf5a5defc596
SergiMPorto <121488207+SergiMPorto@users.noreply.github.com>
2026-09-03 20:06:51 +0100

    fix: invalide caracter

--- backend-api/jenkins/sca/sca.sh
+++ backend-api/jenkins/sca/sca.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "Running SCA scan..."
 
-docker run --rm -v "WORKSPACE/backend-api:/app" -w /app python:3.11-slim bash -c "pip install pip-audit && pip-audit -r requirements.txt"
+docker run --rm -v "$WORKSPACE/backend-api:/app" -w /app python:3.11-slim bash -c "pip install pip-audit && pip-audit -r requirements.txt"
 
 echo "SCA scan completed."
